b08897dcc4f4ba581b45ead5038929fa257bb3c4
Surendra9466 <170303105048@paruluniversity.ac.in>
2020-12-26 20:53:35 +0530

    add interval

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function apiCall(){
+    
     var value = document.getElementById("searchBox").value;
-    console.log(value);
+   
     fetch(`https://en.wikipedia.org/w/api.php?&origin=*&format=json&action=opensearch&search=${value}`)
     .then(response => response.json())
     .then(result => {
@@ -8,14 +9,15 @@ function apiCall(){
         let arrayLink = result[3];
         let innerContent = "";
         arrayLink.forEach(element => {
-            //<div class="card"><div class="container"><li></li></div></div>
+            //<div class="card"><div class="container"><p>Surendra Singh</p></div></div>
             innerContent+=`<li><a href=${element} target="_blank">${element}</a></li>`
         
         });
 
         document.getElementById("result").innerHTML = innerContent;
     })
-
 }
 
-setInterval(apiCall,200);
+document.getElementById("searchBox").oninput = ()=>{
+    apiCall();
+}
